294beef9e87e2803e146dc590a34c2054c7a6d90
Isaiah Guardado <gisaiah416@gmail.com>
2022-07-11 19:22:11 -0700

    Removed comments from goalRoutes.js, created a new model called userModel

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -4,44 +4,6 @@ const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/go
 
 
 router.route('/').get(getGoals).post(setGoal)
-
-/*
-    You can create a seperate function and insert in inside
-    where (req,res) was first located 
-*/
-
-// This was the second alternate version
-// router.get('/', getGoals)
-
-// You can create a function within the body like this
-// router.get('/', (req, res) => {
-//     res.status(200).json({ message: 'Get Goals' })
-// })
-
-// Line 6 takes care of the work for more efficent line usage
-//router.post('/', setGoal)
-
-// router.post('/', (req, res) => {
-//     res.status(200).json({ message: 'Set Goal' })
-// })
-
-
 router.route('/:id').put(updateGoal).delete(deleteGoal)
-
-// Line 6 takes care of the work for more efficent line usage
-// router.put('/:id', updateGoal)
-
-// router.put('/:id', (req, res) => {
-//     res.status(200).json({ message: `Update Goal ${req.params.id}` })
-// })
-
-// Line 6 takes care of the work for more efficent line usage
-// router.delete('/:id', deleteGoal)
-
-// router.delete('/:id', (req, res) => {
-//     res.status(200).json({ message: `Delete Goal ${req.params.id}` })
-// })
-
-
 
 module.exports = router
